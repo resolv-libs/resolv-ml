366a603bc7849951d9e90fba5ccc0c6ffb594844
Matteo Pettenò <matteo.petteno1@gmail.com>
2024-04-23 19:53:28 +0200

    + custom callbacks module

---
+++ src/resolv_ml/training/callbacks.py
@@ -0,0 +1,10 @@
+""" TODO - doc"""
+import keras
+
+
+class LearningRateLoggerCallback(keras.callbacks.Callback):
+    def on_train_batch_end(self, batch, logs=None):
+        logs.update({'lr': self.model.optimizer.learning_rate})
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs.update({'lr': self.model.optimizer.learning_rate})
