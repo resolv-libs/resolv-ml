d2acd34bb445b8ee34e9d4ac14b0ddd28d88ed69
Matteo Pettenò <matteo.petteno1@gmail.com>
2024-04-28 19:21:33 +0200

    [fix]: numpy value for lr logger

--- src/resolv_ml/training/callbacks.py
+++ src/resolv_ml/training/callbacks.py
@@ -4,7 +4,7 @@ import keras
 
 class LearningRateLoggerCallback(keras.callbacks.Callback):
     def on_train_batch_end(self, batch, logs=None):
-        logs.update({'lr': self.model.optimizer.learning_rate})
+        logs.update({'lr': self.model.optimizer.learning_rate.numpy()})
 
     def on_epoch_end(self, epoch, logs=None):
-        logs.update({'lr': self.model.optimizer.learning_rate})
+        logs.update({'lr': self.model.optimizer.learning_rate.numpy()})
